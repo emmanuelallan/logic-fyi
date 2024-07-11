c7b624d6038e540a6e09a78f6f0ab265c79512cb
emmanuelallan <imma.allan@gmail.com>
2024-07-11 17:18:30 +0300

    update the decode jwt

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -17,22 +17,26 @@ const Dashboard = () => {
       return token;
     };
 
+    function parseJwt(t: string) {
+      const base64Url = t.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join(""),
+      );
+
+      return JSON.parse(jsonPayload);
+    }
+
     const token = getTokenFromLocalStorage();
     if (token) {
       try {
-        const base64Url = token.split(".")[1];
-        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-        const jsonPayload = decodeURIComponent(
-          atob(base64)
-            .split("")
-            .map((c) => {
-              return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join(""),
-        );
-
-        const parsedToken = JSON.parse(jsonPayload);
-        setTokenData(parsedToken);
+        setTokenData(parseJwt(token));
       } catch (e) {
         setError("Failed to parse token");
       }
